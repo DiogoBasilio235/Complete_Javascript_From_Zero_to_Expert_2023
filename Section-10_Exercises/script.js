50f94a0e5aebca55ff958e6195de3ef174c806e6
Diogo Basilio <diogobasilio235@gmail.com>
2023-07-28 17:44:55 +0200

    Small steps made

--- Section-10_Exercises/script.js
+++ Section-10_Exercises/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const greet = (greeting) => {
+    return (name) => {
+        console.log(`${greeting} ${name}`);
+    }
+}
+
+// OR
+const greet2 = greeting => name => console.log(`${greeting} ${name}`);
+
+greet("Hello")("Andre");
+greet2("Hello")("Diogo");
+
+const addTax = rate => value => console.log(value + value * rate);
+addTax(0.23)(100);
+
+// OR
+const addTaxRate = function(rate){
+    return function(value){
+        return value + value * rate
+    }
+}
+const addTaxRate23 = addTaxRate(0.23);
+console.log(addTaxRate23(100));
