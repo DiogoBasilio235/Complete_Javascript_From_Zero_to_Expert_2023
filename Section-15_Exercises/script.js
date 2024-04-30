bb875903423a01ff9074cb5c60b17e6454d07628
DiogoBasilio235 <diogobasilio235@gmail.com>
2024-04-30 17:45:21 +0200

    Continue work on Section 15

--- Section-15_Exercises/script.js
+++ Section-15_Exercises/script.js
@@ -1,15 +1,6 @@
 'use strict';
 
 // prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
 
 // HOW TO PLAN A WEB PROJECT
 /**
@@ -33,56 +24,303 @@ const inputElevation = document.querySelector('.form__input--elevation');
  * 5 - Implementation of our plan using code.
  */
 
-// USING THE GEOLOCATION API.
-// It takes 2 arguments, the success callback function and the error callback function.
+class Workout {
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(
-        // Success callback
-        function(position){
-            // Destructuring to create a variable latitude out of the latitude property of "coords" object
-           const {latitude} = position.coords;
-           const {longitude} = position.coords;
-           const coords = [latitude, longitude ]
+   date = new Date();
+   // The ID will be a Date timestamp turned into a string and then we take the last 10 digits.
+   // Generally a bad idea on the real world
+   id = (Date.now() + "").slice(-10);
+   clicks = 0;
 
-           // DISPLAYING A MAP USING LEAFLET LIBRARY
-           // We always need to pass an id of a div. In this case "map".
-           // "L" is the namespace of Leaflet. It takes the coords and the zoom of the map (13)
-           const map = L.map('map').setView(coords, 13);
+   constructor(coords, distance, duration){
+      this.coords = coords; // [lat, lng]
+      this.distance = distance; // in km
+      this.duration = duration; // in minutes
+      
+   }
 
-           // Here it shows the appearance of the map. We can change around and try different layouts
-           L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-           }).addTo(map);
+   _setDescription(){
+      const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+      this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on 
+      ${months[this.date.getMonth()]} ${this.date.getDate()}`
+   }
 
-           // DISPLAYING A MAP MARKER
-           // mapEvent is a special event triggered by Leaflet
-           map.on("click", function(mapEvent) {
-            const {lat, lng} = mapEvent.latlng;
-
-            L.marker([lat, lng])
-            .addTo(map)
-            .bindPopup(L.popup({
-               maxWidth: 250,
-               minWidth: 100,
-               autoClose: false,
-               closeOnClick: false,
-               className: "running-popup",
-               
-            }))
-            .setPopupContent("Workout")
-            .openPopup();
-
-           });
-         },
-        // Error calback 
-        function() {
-        alert("Could not get your position.")
-    });
+   click(){
+      this.clicks++;
+   }
 }
 
-// DISPLAYING A MAP MARKER
+class Running extends Workout{
+   type = "running";
+   constructor(coords, distance, duration, cadence){
+      super(coords, distance, duration);
+      this.cadence = cadence;
+      this.calcPace();
+      this._setDescription();
+   }
+
+   calcPace(){
+      // min per km
+      this.pace = this.duration / this.distance;
+      return this.pace;
+   }
+}
+
+class Cycling extends Workout{
+   type = "cycling";
+   constructor(coords, distance, duration, elevationGain){
+      super(coords, distance, duration);
+      this.elevationGain = elevationGain;
+      this.calcSpeed();
+      this._setDescription();
+   }
+
+   calcSpeed(){
+      // Km/h
+      this.speed = this.distance / (this.duration / 60);
+      return this.pace;
+   }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
+
+
+class App{
+   #map;
+   #mapEvent;
+   #workouts = [];
+   #mapZoomLevel = 13;
+
+   constructor(){
+      // As soon as the object is created, the constructor is executed, making it 
+      //a really suitable place to initiate all the methods needed for the app to work.
+      this._getPosition();
+      
+     // Using again the bind(this) so it doesn't point to the form but to the object itself.
+      form.addEventListener("submit", this._newWorkout.bind(this));
+
+      inputType.addEventListener("change", this._toggleElevationField);
+      containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
+   }
+
+   _getPosition(){
+      if(navigator.geolocation){
+         navigator.geolocation.getCurrentPosition(
+            // Success callback. Using the bind(this) to bind the object to this.
+            this._loadMap.bind(this),
+             // Error calback 
+             function() {
+             alert("Could not get your position.")
+         });
+     }
+   }
+
+   _loadMap(position){
+      
+      // Destructuring to create a variable latitude out of the latitude property of "coords" object
+      const {latitude} = position.coords;
+      const {longitude} = position.coords;
+      const coords = [latitude, longitude ]
+
+      // DISPLAYING A MAP USING LEAFLET LIBRARY
+      // We always need to pass an id of a div. In this case "map".
+      // "L" is the namespace of Leaflet. It takes the coords and the zoom of the map (13)
+      this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
+
+      // Here it shows the appearance of the map. We can change around and try different layouts
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.#map);
+
+      this.#map.on("click", this._showForm.bind(this));
+   }
+
+   _showForm(mapE){
+      // DISPLAYING A MAP MARKER
+      // mapEvent is a special event triggered by Leaflet.
+      // Handling clicks on map
+      // We give value to the global variable "mapEvent", to be used on all the events that happen on the map.
+      this.#mapEvent = mapE;
+      form.classList.remove("hidden");
+      inputDistance.focus();
+   }
+
+   _hideForm() {
+      //Empty inputs
+      inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value 
+         = "";
+      form.style.display = "none";
+      form.classList.add("hidden");
+      setTimeout(() => (form.style.display = "grid"), 1000);
+   }
+
+   _toggleElevationField(){
+      inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+      inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+   }
+
+   _newWorkout(event){
+      // Takes an undetermined number of inputs and checks if every input is a number
+      const validInputs = (...inputs) => inputs.every(ipt => Number.isFinite(ipt));
+      const allPositive = (...inputs) => inputs.every(ipt => ipt > 0);
+
+      // Again, prevent the default behaviour of disapearing the form
+      event.preventDefault();
+
+      // Get data from form
+      const type = inputType.value;
+      const distance = +inputDistance.value;
+      const duration = +inputDuration.value;
+      const {lat, lng} = this.#mapEvent.latlng;
+      let workout;
+
+      // It workout is Running, create Running object
+      if (type === "running") {
+         // Check if data is valid
+         const cadence = +inputCadence.value;
+
+         console.log(distance > 0);
+         console.log(duration > 0);
+         console.log(cadence > 0);
+         if (!validInputs(distance, duration, cadence) || 
+            !allPositive(distance, duration, cadence)
+         ) 
+            return alert("Inputs have to be positive numbers!");
+
+         workout = new Running([lat, lng], distance, duration, cadence);
+      }
+
+      // It workout is Cycling, create Cycling object
+      if (type === "cycling") {
+         // Check if data is valid
+         const elevation = +inputElevation.value;
+
+         if (!validInputs(distance, duration, elevation) || 
+            !allPositive(distance, duration)
+         ) 
+            return alert("Inputs have to be positive numbers!");
+
+         workout = new Cycling([lat, lng], distance, duration, elevation);
+      }
+
+      // Add new object to workout array
+      this.#workouts.push(workout);
+
+      // Render workout on map as marker
+      this._renderWorkoutMarker(workout);
+
+      // Render workout on list
+      this._renderWorkout(workout);
+
+      // Hide form + clear input fields
+      this._hideForm();
+
+   }
+
+   _renderWorkoutMarker(workout){
+      L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+         L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: `${workout.type}-popup`,
+         })
+      )
+      .setPopupContent(`${workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️" } ${workout.description}`)
+      .openPopup();
+   }
+
+   _renderWorkout(workout){
+      let html = `
+      <li class="workout workout--${workout.type}" data-id="${workout.id}">
+      <h2 class="workout__title">${workout.description}</h2>
+      <div class="workout__details">
+        <span class="workout__icon">${
+         workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️" }
+         </span>
+        <span class="workout__value">${workout.distance}</span>
+        <span class="workout__unit">km</span>
+      </div>
+      <div class="workout__details">
+        <span class="workout__icon">⏱</span>
+        <span class="workout__value">${workout.duration}</span>
+        <span class="workout__unit">min</span>
+      </div>
+      `;
+
+      if (workout.type === "running"){
+         html += `<div class="workout__details">
+         <span class="workout__icon">⚡️</span>
+         <span class="workout__value">${workout.pace.toFixed(1)}</span>
+         <span class="workout__unit">min/km</span>
+       </div>
+       <div class="workout__details">
+         <span class="workout__icon">🦶🏼</span>
+         <span class="workout__value">${workout.cadence}</span>
+         <span class="workout__unit">spm</span>
+       </div>
+     </li>
+     `;
+      }
+
+      if (workout.type === "cycling"){
+         html += `
+      <div class="workout__details">
+         <span class="workout__icon">⚡️</span>
+         <span class="workout__value">${workout.speed.toFixed(1)}</span>
+         <span class="workout__unit">km/h</span>
+       </div>
+       <div class="workout__details">
+         <span class="workout__icon">⛰</span>
+         <span class="workout__value">${workout.elevationGain}</span>
+         <span class="workout__unit">m</span>
+       </div>
+     </li>
+     `;
+      }
+
+      form.insertAdjacentHTML("afterend", html);
+   }
+
+   _moveToPopup(event){
+      // We search for an event on the closest class called "workout"
+      const workoutElement = event.target.closest(".workout");
+      if (!workoutElement) return;
+
+      // We then compare the details present on the html with the workouts we have stored on our code
+      // to be used in building the setView()
+      const workout = this.#workouts.find(workout => workout.id === workoutElement.dataset.id);
+      
+      // setView() is a special function of Leaflet Library
+      this.#map.setView(workout.coords, this.#mapZoomLevel, {
+         animate: true,
+         pan: {
+            duration: 1
+         },
+      });
+
+      // Using the public interface
+      workout.click();
+   }
+}
+
+const app = new App();
+
+
+
 
 
 
