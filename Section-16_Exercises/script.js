9d75c0e50516a9ecec83de809c5e3f9a8f45c38e
DiogoBasilio235 <diogobasilio235@gmail.com>
2024-05-31 18:02:53 +0200

    More progress made on unit 16

--- Section-16_Exercises/script.js
+++ Section-16_Exercises/script.js
@@ -21,6 +21,11 @@ const renderCountry = function(data, className = ""){
   countriesContainer.style.opacity = 1;
 }
 
+const renderError = function(msg){
+    countriesContainer.insertAdjacentText("beforeend", msg);
+    //countriesContainer.style.opacity = 1;
+}
+
 // FIRST REQUEST
 // Old school way
 /*
@@ -29,7 +34,7 @@ const getCountryData = function(country){
     request.open("GET", countriesURL + country);
     //request.setRequestHeader("Authorization", "Bearer " + token);
     request.send();
-    
+
     // We add an eventListener to wait for the "load" event
     request.addEventListener("load", function(){
         // The this keyword inside of the function is the request
@@ -63,7 +68,7 @@ const getCountryAndNeighbourData = function(country){
     const request = new XMLHttpRequest();
     request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
     request.send();
-    
+
     // We add an eventListener to wait for the "load" event
     request.addEventListener("load", function(){
         // The this keyword inside of the function is the request
@@ -75,7 +80,7 @@ const getCountryAndNeighbourData = function(country){
         // Render country 2
         const neighbour = data.borders?.[0];
 
-        if(!neighbour) return; 
+        if(!neighbour) return;
         const request2 = new XMLHttpRequest();
         request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
         request2.send();
@@ -109,7 +114,7 @@ const getCountryData = function(country){
     fetch(`https://restcountries.com/v3.1/name/${country}`).then(function(response){
 
         // The json() is available on all the response objects that is coming from the fetch function.
-        return response.json(); 
+        return response.json();
 
     }).then(function(data){ // The json() is also an Async function. So to read the data we call again another then()
         renderCountry(data[0]);
@@ -119,32 +124,268 @@ const getCountryData = function(country){
 getCountryData("portugal");
 */
 
-// CHAINING PROMISES
+// CHAINING PROMISES &&
 // Using arrow functions
-const getCountryData = function(country){
-    fetch(`https://restcountries.com/v3.1/name/${country}`)
-        .then(response => response.json())
-        .then(data => {
-            renderCountry(data[0]);
 
-            //Getting just one neighbour
-            const neighbour = data[0].borders?.[0];
-            if (!neighbour) return;
 
-            //Flat chain of promises
-            return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
-            .then(response => response.json())
-            .then(data => renderCountry(data[0], "neighbour"));
 
-            // Getting all neighours
-            // const neighbours = data[0].borders;
-            // if (!neighbours) return;
+// const getCountryData = function(country){
+//     fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response =>  // Rejected promise
+//         {
+//             if (!response.ok)
+//                 throw new Error(`Country not found (${response.status}) Try again!`)
 
-            // neighbours.forEach(ctr => {
-            //     return fetch(`https://restcountries.com/v3.1/alpha/${ctr}`)
-            //     .then(response => response.json())
-            //     .then(data => renderCountry(data[0], "neighbour"));
-            // });
-        })
-};
-getCountryData("germany");
+//             return response.json()
+//         })
+//     .then(data => {
+//         renderCountry(data[0]);
+
+//         //Getting just one neighbour
+//         const neighbour = data[0].borders?.[0];
+
+//         if (!neighbour) return;
+
+//         return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then(response => response.json())
+//     .then(data => renderCountry(data[0], "neighbour"))
+//     .catch(err => {  // error handling
+//         console.error(`${err} 💣💣💣`);
+//         renderError(`Something went wrong ${err.message}. Try again!`)
+//     })
+//     .finally(() => { // No matter what it happens in the code, this function is always executed
+//         countriesContainer.style.opacity = 1; // This is use in order to show
+//     });
+// }
+
+// const getCountryData = function(country){
+//     getJson(`https://restcountries.com/v3.1/name/${country}`, "Country not found")
+//         .then(data => {
+//         renderCountry(data[0]);
+
+//         //Getting just one neighbour
+//         const neighbour = data[0].borders?.[0];
+
+//         if (!neighbour) throw new Error("No neighbour found.");
+
+//         return getJson(`https://restcountries.com/v3.1/alpha/${neighbour}`, "Country not found");
+//     })
+//     .then(data => renderCountry(data[0], "neighbour"))
+//     .catch(err => {  // error handling
+//         renderError(`Something went wrong ${err.message}. Try again!`)
+//     })
+//     .finally(() => { // No matter what it happens in the code, this function is always executed
+//         countriesContainer.style.opacity = 1; // This is use in order to show
+//     });
+// }
+
+
+// const getJson = function(url, errorMsg = "Something went wrong"){
+//     return fetch(url)
+//     .then(response =>  // Rejected promise
+//     {
+//         if (!response.ok)
+//             throw new Error(`${errorMsg} (${response.status})`);
+
+//         return response.json()
+//     })
+// }
+
+// btn.addEventListener("click",function() {
+//     getCountryData("australia");
+// })
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+// My solution
+// A different approach was taken for the sake of trying something different.
+// const renderGeoLocationData = function(data, className = ""){
+//     const html =`
+//     <article class="country ${className}">
+//     <div class="country__data">
+//       <h3 class="country__name">Country: ${data.country}</h3>
+//       <p class="country__row"><span>📍</span>City: ${data.city}</p>
+//       <p class="country__row"><span>🏔️</span>Elevation: ${data.elevation} metres</p>
+//       <p class="country__row"><span>🗺️</span>Lat / Long: ${data.latt} /  ${data.longt}</p>
+//     </div>
+//   </article>`;
+//   countriesContainer.insertAdjacentHTML("beforeend", html);
+//   //countriesContainer.style.opacity = 1;
+// }
+
+// const whereAmI = function(lat, long){
+//     fetch(`https://geocode.xyz/${lat},${long}?json=1&auth=239839308822961272590x51664`)
+//     .then(response =>
+//         {
+//             if (!response.ok)
+//                 throw new Error(`Coordinates not found (${response.status}) Try again!`)
+
+//             return response.json()
+//         })
+//     .then(data => {
+//         console.log(`You are in ${data.city}, ${data.country}`)
+//         renderGeoLocationData(data)
+//     })
+//     .catch(err => {
+//         console.error(`Something went wrong ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//             countriesContainer.style.opacity = 1;
+//     });
+// }
+
+// //whereAmI(52.508, 13.381);
+// //whereAmI( 19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+
+//Teacher solution
+// const whereAmI = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json&auth=239839308822961272590x51664`)
+//   .then(res => {
+//     if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+//     return res.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//     console.log(`You are in ${data.city}, ${data.country}`);
+
+//     return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+//   })
+//   .then(res => {
+//     if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+//     return res.json();
+//   })
+//   .then(data => renderCountry(data[0]))
+//   .catch(err => console.error(`${err.message} 💥`));
+// };
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+
+// THE EVENT LOOP
+// JavaScript is single-threaded, meaning it can execute one task at a time. However,
+// it can handle asynchronous operations like network requests, timers, and I/O operations efficiently.
+// The Event Loop is crucial in managing these asynchronous operations without blocking the main thread.
+//
+// Components of the Event Loop
+// Call Stack: This is where JavaScript keeps track of function calls.
+// When a function is called, it's added to the stack. When the function completes, it's removed from the stack.
+
+// Web APIs: These are provided by the browser (or the Node.js runtime) and include functionalities like
+// setTimeout, DOM events, fetch, etc. These APIs handle asynchronous tasks.
+
+// Task Queue (Callback Queue): When an asynchronous operation completes,
+// its callback function is added to this queue, waiting to be executed.
+
+// Event Loop: This is the mechanism that checks if the call stack is empty and if there are any pending callbacks
+// in the task queue. If the stack is empty, it moves the first callback from the task queue to the call stack, executing it.
+
+
+// console.log("Test start");
+// setTimeout(() => console.log("0 second timer"), 0);
+// Promise.resolve("Resolved promise 1").then(res => console.log(res));
+// console.log("Test end");
+
+// In this example, the console will look like this:
+// line 1 -> Test start
+// line 2 -> Test end
+// line 3 -> Resolved promise 1
+// line 4 -> 0 second timer
+
+// The code outside of any callback is executed first. (line 1 & line 2)
+// Both the timer and the Promise will finish at the same time. But the timer finishe first
+// and it's callback will bu put on the callback queue first.
+// But it wont be logged to the console first because of the micro-tasks queue. The call back from the Promise
+// will be put in the micro-tasks queue, and it has priority over the callback queue.
+// If any of the micro-tasks takes a lot of time to complete, then the timer will be delayed and not run after the zero seconds,
+// only when the micro-task is actually done with it's work.
+
+
+// In this case the Promise 2 is still resolved immediatly but the micro-task that it contains will take longer.
+// This will make the callback queue be delayed and not run after the zero seconds
+// console.log("Test start");
+// setTimeout(() => console.log("0 second timer"), 0);
+// Promise.resolve("Resolved promise 1").then(res => console.log(res));
+// Promise.resolve("Resolved promise 2").then(res =>
+//     {
+//         for(let i = 0; i < 1000000000; i++ ){}
+//         console.log(res)
+//     }
+// );
+// console.log("Test end");
+
+// line 1 -> Test start
+// line 2 -> Test end
+// line 3 -> Resolved promise 1
+// line 4 -> Resolved promise 2
+// line 5 -> 0 second timer
+
+
+// BUILDING A SIMPLE PROMISE
+// The Promise object takes only one argument calle "Executor function".
+// This function has the "resolve" and "reject" parameters.
+// const lotteryPromise = new Promise(function(resolve, reject){
+//     console.log("Lottery draw is happening...");
+
+//     setTimeout(function(){
+//         if (Math.random() >= 0.5) {
+//             // In order to fulfill the Promise we use the resolve().
+//             // Whatever value we pass into the resolve() is gonna be the result of the Promise
+//             // that will be available in the then() handler.
+//             resolve("You WON the lottery!!! 🤑🤑🤑");
+//         } else {
+//             reject(
+//                 // Creating an error object is the way to go
+//                 new Error("You lost your money!💩💩💩")
+//             );
+//         }
+//     }, 2000)
+// });
+
+// lotteryPromise.then(res => console.log(res))
+// .catch(err => console.error(err));
+
+// Promissifying setTimout
+// const wait = function(seconds){
+//     return new Promise(function(resolve){
+//         setTimeout(resolve, seconds * 1000);
+//     });
+// }
+
+// wait(2).then(() => {
+//     console.log("I've waited for 2 seconds");
+//     return wait(1);
+// }).then(() => {
+//     console.log("I've waited for 1 second");
+// });
+
+// Promise.resolve("abc").then(x => console.log(x));
+// Promise.reject(new Error("Problem")).catch(x => console.error(x));
