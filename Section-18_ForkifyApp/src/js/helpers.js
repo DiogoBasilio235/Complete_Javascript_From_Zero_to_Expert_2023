345443c4695e437e6238e05c53da89d421024fdb
Diogo Basilio <d.basilio@esis-italia.com>
2024-07-05 08:58:14 +0200

    More progress made on section 18

--- Section-18_ForkifyApp/src/js/helpers.js
+++ Section-18_ForkifyApp/src/js/helpers.js
@@ -9,32 +9,18 @@ const timeout = function (s) {
     });
   };
 
-export const getJson = async function(url) {
-    try{
-      const fetchPro = fetch(url);
-      const res = await Promise.race([fetchPro, timeout(TIMEOUT_SECONDS)]);
-      const data = await res.json();
-  
-      if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-      return data;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export const sendJson = async function(url, uploadData) {
+export const AJAX = async function(url, uploadData = undefined){
   try{
-    const fetchPro = fetch(url,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(uploadData)
-      }
-    );
+    const fetchPro = uploadData ? fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(uploadData),
+    }) :  fetch(url);
+
+    
+
     const res = await Promise.race([fetchPro, timeout(TIMEOUT_SECONDS)]);
     const data = await res.json();
   
@@ -42,7 +28,45 @@ export const sendJson = async function(url, uploadData) {
 
     return data;
   }
-  catch(err){
-      throw err;
-  }
+    catch(err){
+        throw err;
+    }
 }
+
+// export const getJson = async function(url) {
+//     try{
+//       const fetchPro = fetch(url);
+//       const res = await Promise.race([fetchPro, timeout(TIMEOUT_SECONDS)]);
+//       const data = await res.json();
+  
+//       if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+//       return data;
+//     }
+//     catch(err){
+//         throw err;
+//     }
+// }
+
+// export const sendJson = async function(url, uploadData) {
+//   try{
+//     const fetchPro = fetch(url,
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(uploadData)
+//       }
+//     );
+//     const res = await Promise.race([fetchPro, timeout(TIMEOUT_SECONDS)]);
+//     const data = await res.json();
+  
+//     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+//     return data;
+//   }
+//   catch(err){
+//       throw err;
+//   }
+// }
